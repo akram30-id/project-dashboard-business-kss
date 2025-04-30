e275d14ab2f73e3bd1aaee619a2e4b8adffd6723
akram30-id <afalerin2016@gmail.com>
2025-04-30 15:29:48 +0700

    update: color black

--- resources/views/components/dashboard/stats-card.blade.php
+++ resources/views/components/dashboard/stats-card.blade.php
@@ -1,7 +1,7 @@
 <div class="col-lg-4">
     <div class="stats-card {{ $type }} d-flex justify-content-between align-items-center" style="height: 120px;">
         <div>
-            <h4 style="font-weight: bold;" id="{{ $id }}">{{ $value }}</h4>
+            <h4 style="font-weight: bold; color: #000" id="{{ $id }}">{{ $value }}</h4>
             <p><i class="{{ $chartIcon }} me-1"></i> {{ $label }} <span class="year-label">{{ $year }}</span></p>
         </div>
         <div class="icon">
